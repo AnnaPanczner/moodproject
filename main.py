d855614b309ca9f9780329c5fdf4496f0bede501
Anna Panczner <annapanczner@gmail.com>
2021-12-07 19:51:00 -0500

    users can now GET and POST mood ratings for a given email. accessing the endpoint is authenticated

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from functools import wraps
 import firebase_admin as fba
+from firebase_admin import db
 from firebase_admin import auth
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, render_template, request, jsonify
 import pyrebase
 import json
 import secrets
+from json import loads
+from datetime import date
 
 app = Flask(__name__, template_folder='./templates',static_folder='./static')
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -12,6 +16,19 @@ app.secret_key = secret
 cred = fba.credentials.Certificate('firebaseadmin.json')
 firebase = fba.initialize_app(cred)
 pb = pyrebase.initialize_app(json.load(open('firebaseconfig.json')))
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            user = auth.verify_id_token(request.headers['authorization'])
+            request.user = user
+        except:
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
 
 @app.route('/')
 def home():
@@ -23,7 +40,7 @@ def login():
 
 #Api route to sign up a new user
 @app.route('/loginsignup', methods=['GET','POST'])
-def signup():
+def loginsignup():
     email = request.form.get('email')
     password = request.form.get('password')
     if email is None or password is None:
@@ -45,6 +62,54 @@ def signup():
             except:
                 return jsonify(status="exists", response="Error logging in.")
         return jsonify(status="fail", response=str(e))
+
+@app.route('/mood', methods=['GET', 'POST'])
+@check_token
+def mood():
+    email = ""
+    ref = db.reference(url="https://mood-c9d58-default-rtdb.firebaseio.com/")
+    returnHTML = "<p>"
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        rating = request.form.get('rating')
+        d = date.today()
+
+        ref.push().set({
+            'email': email,
+            'year': d.year,
+            'month': d.month,
+            'day': d.day,
+            'rating': rating
+        })
+    elif request.method == 'GET':
+        email = request.args.get('email')
+
+    rawJSON = ref.get()
+    for entry in rawJSON:
+        rating = ref.child(str(entry)).get()
+        if(ref.child(str(entry)).child("email").get() == email):
+            returnHTML = returnHTML + str(rating) + "<br>"
+    returnHTML = returnHTML + "</p>"
+    returnHTML = returnHTML + "<input type = 'text' placeholder = 'Enter your mood rating' id = 'rating'>"
+    returnHTML = returnHTML + "<input type='submit' onclick='rate()' value='Post Mood Rating'\>"
+    returnHTML = returnHTML + "<script>"
+    returnHTML = returnHTML +  "function rate() {" \
+          "var r = document.getElementById('rating').value; $.ajax({ \
+          url: '/mood', \
+          type: 'POST', \
+          data: {email: localStorage.getItem('email'), rating: r}, \
+          dataType: 'text', \
+          headers: {'Authorization': localStorage.getItem('token')}, \
+           success: function(returnData) { \
+                window.history.pushState('', '', '/mood'); \
+                document.open(); \
+                document.write(returnData); \
+                document.close(); \
+           } \
+        }); } </script>"
+
+    return(returnHTML)
 
 if __name__ == '__main__':
     app.run() #run app (for testing, on localhost)
